630714ecd825c16513664a9c454f8e07c6ea1c71
Boris Moskov <borisstmoskov@gmail.com>
2026-09-22 16:14:21 +0300

    implement Employees controller

--- EmployeesCollaborationTracker/Controllers/EmployeesController.cs
+++ EmployeesCollaborationTracker/Controllers/EmployeesController.cs
@@ -1,6 +1,39 @@
-﻿namespace EmployeesCollaborationTracker.Controllers
+﻿using EmployeesCollaborationTracker.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesCollaborationTracker.Controllers
 {
-    public class EmployeesController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : ControllerBase
     {
+        private readonly IEmployeeFileReader _fileReader;
+        private readonly ICollaborationService _collaborationService;
+
+        public EmployeesController(IEmployeeFileReader fileReader, ICollaborationService collaborationService)
+        {
+            _fileReader = fileReader;
+            _collaborationService = collaborationService;
+        }
+
+        [HttpPost("collaborations")]
+        public IActionResult GetCollaborations(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file was uploaded.");
+            }
+
+            List<Domain.Entities.EmployeeProject> employeeProjects;
+
+            using (var stream = file.OpenReadStream())
+            {
+                employeeProjects = _fileReader.Read(stream);
+            }
+
+            var result = _collaborationService.GetCollaboratingPairs(employeeProjects);
+
+            return Ok(result);
+        }
     }
 }
--- EmployeesCollaborationTracker/Program.cs
+++ EmployeesCollaborationTracker/Program.cs
@@ -1,7 +1,28 @@
+using EmployeesCollaborationTracker.Application.Interfaces;
+using EmployeesCollaborationTracker.Application.Services;
+using EmployeesCollaborationTracker.Infrastructure.FileReaders;
+using EmployeesCollaborationTracker.Infrastructure.Parsing;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IDateParser, DateParser>();
+builder.Services.AddScoped<IEmployeeFileReader, CsvEmployeeFileReader>();
+builder.Services.AddScoped<ICollaborationService, CollaborationService>();
+
+var clientOrigin = "http://localhost:4200";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("EmployeesCollaborationTrackerClient", policy =>
+    {
+        policy.WithOrigins(clientOrigin)
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -11,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("EmployeesCollaborationTrackerClient");
 
 app.UseAuthorization();
 
